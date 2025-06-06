1303dc9802f21dc7a538f50b477bf6b09c1f30ec
Ian Lucca <ianluccaac@gmail.com>
2025-06-06 08:12:18 -0300

    JAVASCRIPT não e vida

--- js/teste.js
+++ js/teste.js
@@ -23,3 +23,36 @@ document.getElementById("quantidadeHeader").addEventListener("click", () => {
     ascending ? "&#x25B2;" : "&#x25BC;"
   }`;
 });
+
+let ascending1 = true;
+
+document.getElementById("valorHeader").addEventListener("click", () => {
+  const table = document.getElementById("tableProductos");
+  const rows = Array.from(table.querySelectorAll("tr")).slice(1);
+
+  rows.sort((a, b) => {
+    const getValor = (row) =>
+      parseFloat(
+        row.children[1].textContent
+          .replace("R$ ", "")
+          .replace(/\./g, "")
+          .replace(",", ".")
+      );
+    const valorA = getValor(a);
+    const valorB = getValor(b);
+
+    return ascending1 ? valorA - valorB : valorB - valorA;
+  });
+
+  // Reinsere as linhas ordenadas na tabela
+  rows.forEach((row) => table.appendChild(row));
+
+  ascending1 = !ascending1;
+
+  // Atualiza o texto do cabeçalho
+  document.getElementById("valorHeader").innerHTML = `Valor ${
+    ascending1 ? "&#x25B2;" : "&#x25BC;"
+  }`;
+});
+
+
